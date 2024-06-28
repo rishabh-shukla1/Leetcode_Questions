a4ea991275f8aa58ef7f5b29aea5132e527b0e38
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2024-06-28 17:24:17 +0530

    Time: 2 ms (33.09%) | Memory: 44.3 MB (50.28%) - LeetSync

---
+++ 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+
+
+         int a[]=new int[arr.length];
+         for(int i=0;i<arr.length;i++)
+         {
+            a[i]=arr[i];
+         }
+
+         Arrays.sort(a);
+
+         int sum=0;
+         int usum=0;
+         int ans=0;
+
+         for(int i=0;i<a.length;i++)
+         {
+            sum=sum+a[i];
+            usum=usum+arr[i];
+
+            if(sum==usum)
+            {
+                ans++;
+            }
+         }
+
+         return ans;
+
+    }
+}
