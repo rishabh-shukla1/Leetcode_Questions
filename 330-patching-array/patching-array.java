8771001709efd7761aa92a76e084020c72fb5fed
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2024-06-29 21:29:25 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (5.51%) - LeetSync

---
+++ 330-patching-array/patching-array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minPatches(int[] nums, int n) {
+
+
+        long miss=1;
+        int i=0;
+        int ans=0;
+
+        while(miss<=n)
+        {
+             if(i<nums.length && nums[i]<=miss)
+             {
+                miss=miss+nums[i++];
+
+
+             }
+             else
+             {
+                miss=miss+miss;
+                ans++;
+             }
+
+        }
+
+        return ans;
+        
+    }
+}
