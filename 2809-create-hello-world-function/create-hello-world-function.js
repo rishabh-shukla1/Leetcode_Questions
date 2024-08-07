fc69348f3336d6bbd1d93e5fcfe9c8c676b17f25
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2024-08-07 15:21:13 +0530

    Time: 48 ms (71.07%) | Memory: 48.6 MB (69.79%) - LeetSync

---
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -0,0 +1,18 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+
+        return "Hello World"
+        
+    }
+};
+
+console.log(createHelloWorld());
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
