f5d29604f78ce18afcb00b40a31a72b0f030ab0b
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2024-09-21 11:36:32 +0530

    Time: 750 ms (5.13%) | Memory: 54.1 MB (5.13%) - LeetSync

---
+++ 386-lexicographical-numbers/lexicographical-numbers.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+
+
+        HashSet<Integer> l=new HashSet<>();
+        List<Integer> ll=new ArrayList<>();
+     
+    for(int i=1;i<=n;i++)
+    {
+        find(n,l,String.valueOf(i),ll);
+    }
+
+   
+
+        return ll;
+        
+    }
+
+    public static void find(int n ,HashSet<Integer>l,String s,List<Integer>ll)
+    {
+        if( Integer.parseInt(s)>n)
+        {
+            return ;
+        }
+        if(!l.contains(Integer.parseInt(s)))
+        {
+        l.add(Integer.parseInt(s));
+        ll.add(Integer.parseInt(s));
+        }
+
+
+        for(int j=0;j<=13;j++)
+        {
+            
+            find(n,l,s+Integer.toString(j),ll);
+            
+
+        }
+
+        
+
+
+    }
+}
