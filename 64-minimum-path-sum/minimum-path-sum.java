2eddd01734d89ff85b0d4af2f26e0e41e2142694
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2024-11-24 02:19:19 +0530

    Time: 1 ms (99.76%) | Memory: 47.6 MB (45.37%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -7,32 +7,34 @@ class Solution {
       {
         Arrays.fill(a,-1);
       }
-        return Path_Sum(grid,0,0,dp);
+        return find(grid.length-1 , grid[0].length-1,grid, dp);
         
     
     }
 
-    public static int Path_Sum(int[][]grid , int cr, int cc,int dp[][])
+    public  static int find(int i, int j , int grid[][],int dp[][])
     {
-        if(cr==grid.length-1 && cc== grid[0].length-1)
+        if(i==0 && j==0)
         {
-            return grid[cr][cc];
-
+            return grid[i][j];
         }
-       
-        if(cr>=grid.length || cc>=grid[0].length)
+
+        if(i<0 || j<0)
         {
             return Integer.MAX_VALUE;
         }
-
-         if(dp[cr][cc]!=-1)
+        if(dp[i][j]!=-1)
         {
-            return dp[cr][cc];
+            return dp[i][j];
         }
+        
+         int up=find(i-1,j,grid,dp);
+         int down=find(i,j-1,grid,dp);
 
-        int right= Path_Sum(grid,cr,cc+1,dp);
-        int down = Path_Sum(grid,cr+1,cc,dp);
+         return dp[i][j]=Math.min(up,down)+grid[i][j];
 
-        return  dp[cr][cc]=Math.min(right,down)+grid[cr][cc];
+
     }
+
+   
 }
