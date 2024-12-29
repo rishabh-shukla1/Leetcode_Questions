98c52d7aed70b6ff242983807588a14c3708a798
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2024-12-29 19:31:25 +0530

    Time: 8 ms (35.01%) | Memory: 50.5 MB (5.01%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        
+ 
+    Integer dp[][][]=new Integer[prices.length][2][k+1];
+
+
+
+
+
+
+      return find(prices,1,k,0,dp);
+        
+    }
+
+    public static int find(int p[],int buy,int cap,int i,Integer dp[][][])
+    {
+        if(cap==0)
+        {
+            return 0;
+        }
+        if(i==p.length)
+        {
+            return 0;
+        }
+        if(dp[i][buy][cap]!=null)
+        {
+            return dp[i][buy][cap];
+        }
+        
+
+
+        int profit=0;
+        if(buy==1)
+        {
+
+            profit=Math.max(-p[i]+find(p,0,cap,i+1,dp),0+find(p,1,cap,i+1,dp));
+        
+            
+        }
+        else
+        {
+            profit=Math.max(p[i]+find(p,1,cap-1,i+1,dp),0+find(p,0,cap,i+1,dp));
+
+        }
+
+
+        return dp[i][buy][cap]=profit;
+    }
+}
