22f24567907368e32522527724fff3450e37b678
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2024-12-31 22:21:08 +0530

    Time: 1 ms (99.28%) | Memory: 42.7 MB (12.69%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isBalanced(String num) {
+
+        int odd=0;
+        int even=0;
+
+        for(int i=0;i<num.length();i++)
+        {
+            if(i%2==0)
+            {
+                even=even+(int)num.charAt(i)-48;
+            }
+            else
+            {
+                odd=odd+(int)num.charAt(i)-48;
+
+            }
+        }
+
+        if(odd==even)
+        {
+            return true;
+        }
+
+        return false;
+        
+    }
+}
