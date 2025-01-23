4c94a9a5bcdca5a0c41e57569e06ffe123461f59
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2025-01-23 23:52:27 +0530

    Time: 1 ms (94.40%) | Memory: 45.6 MB (28.36%) - LeetSync

---
+++ 419-battleships-in-a-board/battleships-in-a-board.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int countBattleships(char[][] board) {
+         int c=0;
+
+         
+
+        for(int i=0;i<board.length;i++)
+        {
+            for(int j=0;j<board[0].length;j++)
+            {
+                if(board[i][j]=='X')
+                {
+                    dfs(i,j,board);
+                    c++;
+                }
+            }
+        }
+
+        return c;
+        
+    }
+
+    public static void dfs(int i,int j, char b[][])
+    {
+
+        if(i<0 || j<0 || i>=b.length || j>=b[0].length || b[i][j]=='.')
+        {
+           return ;
+
+
+        }
+
+        b[i][j]='.';
+
+        
+    
+
+
+
+        dfs(i+1,j,b);
+        dfs(i,j+1,b);
+        
+    }
+}
