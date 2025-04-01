61263ee2cf9978ee15b726c7bca0c79b51b107a2
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2025-04-02 01:42:47 +0530

    Time: 9 ms (46.09%) | Memory: 42.9 MB (26.20%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.java
+++ 273-integer-to-english-words/integer-to-english-words.java
@@ -1,57 +1,68 @@
 class Solution {
-      String  less_20[]={"","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
-
-
-        String ten[]={"","","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
-
-        String thousand[]={"","Thousand","Million","Billion"};
     public String numberToWords(int num) {
-
-     
-      
 
 
         if(num==0)
-        return "Zero";
+        {
+            return "Zero";
+        }
 
-        String w="";
-        int i=0;
+
+        String less_20[]={"","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+
+        String tens[]={"","","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
+
+        String  t[]={"","Thousand","Million","Billion"};
+
+
+        int group=0;
+
+        String ans="";
 
         while(num>0)
         {
+
             if(num%1000 !=0)
             {
-                w=make(num%1000)+thousand[i]+" "+w;
+                String temp="";
+
+                int val=num%1000;
+
+                if(val>=100)
+                {
+
+                    temp=temp+less_20[val/100]+" Hundred ";
+                    val=val%100;
+                }
+                if(val>=20)
+                {
+                    temp=temp+tens[val/10]+" ";
+                    val=val%10;
+
+                }
+
+                if(val>0)
+                {
+                    temp=temp+less_20[val]+" ";
+                }
+
+
+                temp=temp+t[group]+" ";
+
+                ans=temp+ans;
+
+
+
+
 
 
             }
 
             num=num/1000;
-            i++;
-
+            group++;
         }
 
-
-        return w.trim();
+        return ans.trim();
         
-
-     
-    }
-
-    public String make(int num)
-    {
-        if(num==0)
-        return "";
-        else if(num<20)
-        return less_20[num]+" ";
-        else if(num<100)
-        {
-            return ten[num/10]+" "+make(num%10);
-        }
-        else
-        {
-            return less_20[num/100]+" Hundred "+make(num%100);
-        }
-
     }
 }
